6ddbaf758ca29bd563f3c8f236be489976d0cd50
Aishwarya R <134778234+aish-r-01@users.noreply.github.com>
2024-11-13 09:38:44 +0530

    Add files via upload

---
+++ jenkin.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class jenkin
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+// System.out.println("Enter your name:");
+// String name=sc.next();
+System.out.println("Welcome ");
+}
+}
